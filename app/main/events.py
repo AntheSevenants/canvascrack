3e5bf6b9b7795539fc7457888f65f5d46679eb07
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-27 19:06:50 +0100

    Start implementing netcode

--- app/main/events.py
+++ app/main/events.py
@@ -5,4 +5,18 @@ from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 
+from flask import current_app
+
 namespace = ""
+
+
+@socketio.on('connect', namespace=namespace)
+def io_connect():
+    print("Client connected")
+    broadcast_state()
+
+
+def broadcast_state():
+    game = current_app.config["game"]
+
+    socketio.emit("state", game.as_dict())
