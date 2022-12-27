0c5b9cacfb860c1a9036dc909ad3b9550a47519f
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-27 20:41:30 +0100

    Grey out unchosen options after answering

--- app/static/js/misc/questions.js
+++ app/static/js/misc/questions.js
@@ -1,14 +1,24 @@
 class Questions {
     static renderState(state) {
+        let challengerResponseIndex = -1;
+        if (state.crack_response_time) {
+            challengerResponseIndex = state.challenger_response_history[state.challenger_response_history.length - 1];
+        }
+
         for (let i = 0; i < state.current_question.answers.length; i++) {
             const answer = state.current_question.answers[i];
             const answerElement = document.getElementById(`question_option_inner_${i}`);
-            
+
             answerElement.innerHTML = answer;
 
+            const answerElementOuter = document.getElementById(`question_option_${i}`);
             if (host) {
-                const answerElementOuter = document.getElementById(`question_option_${i}`);
                 answerElementOuter.onclick = () => { canvascrack.challengerRespond(i); };
+            }
+
+            answerElementOuter.classList.remove("greyed");
+            if (state.crack_response_time && i != challengerResponseIndex) {
+                answerElementOuter.classList.add("greyed");
             }
         }
 
