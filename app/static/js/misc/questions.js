a709a47bb30f419b3b915dace86e6c10fcd79ffa
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-12-29 11:53:59 +0100

    Prepare crack follow box rendering

--- app/static/js/misc/questions.js
+++ app/static/js/misc/questions.js
@@ -1,8 +1,16 @@
 class Questions {
     static renderState(state) {
+        const gameElement = document.getElementById("game");
+        gameElement.classList.remove("reviewing");
+
         let challengerResponseIndex = -1;
         if (state.crack_response_time) {
             challengerResponseIndex = state.challenger_response_history[state.challenger_response_history.length - 1];
+        }
+
+        if (state.review_stage) {
+            challengerResponseIndex = state.challenger_response_history[state.current_subround];
+            gameElement.classList.add("reviewing");
         }
 
         for (let i = 0; i < state.current_question.answers.length; i++) {
@@ -17,8 +25,17 @@ class Questions {
             }
 
             answerElementOuter.classList.remove("greyed");
-            if (state.crack_response_time && i != challengerResponseIndex) {
+            if ((state.crack_response_time || state.review_stage) && i != challengerResponseIndex) {
                 answerElementOuter.classList.add("greyed");
+            }
+
+            if (state.review_stage) {
+                const crackResponseElement = document.getElementById(`question_crack_response_${i}`);
+                crackResponseElement.innerHTML = "&nbsp;"
+
+                if (i == challengerResponseIndex) {
+                    // todo conditionally show crack response
+                }
             }
         }
 
